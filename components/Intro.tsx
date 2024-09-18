15503577050e7b614db6c5058583e52685fefb67
Pramesh Basnet <bpramesh742@gmail.com>
2024-09-18 23:55:12 +0545

    initial commit

---
+++ components/Intro.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import ProfilePic from "@/app/images/asd.jpeg";
+
+export default function Intro() {
+  return (
+    <div className="w-full flex flex-col-reverse lg:flex-row gap-14 justify-between items-center -z-10">
+      <div className="flex flex-col gap-2 w-full lg:w-4/5">
+        <h1 className="text-4xl font-bold mb-4">
+          Hi! I&apos;m Pramesh Basnet.
+        </h1>
+        <p className="text-base text-gray-800 dark:text-gray-300">
+          Seasoned Fullstack Engineer with 1+ years in industry. I excel in
+          building applications from scratch to production-ready. My expertise
+          spans across frontend, backend, and full-stack development.
+        </p>
+      </div>
+      <div className="flex items-center justify-center md:mb-0">
+        <Image
+          src={ProfilePic}
+          alt="Yatharth"
+          width={160}
+          height={160}
+          className="border-2 border-gray-100 rounded-full object-cover"
+        />
+      </div>
+    </div>
+  );
+}
---
+++ components/Skills.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const skillsData = [
+  "React",
+  "Next.js",
+  "Tailwind CSS",
+  "JavaScript",
+  "HTML",
+  "CSS",
+  "Git",
+  "GitHub",
+  "PostgreSQL",
+  "MySQL",
+  "RESTful APIs",
+  "C#",
+  ".NET",
+  "Flutter",
+];
+
+function Skills() {
+  return (
+    <div className="flex flex-col gap-4 w-full">
+      <h1 className="text-2xl font-bold">Skills</h1>
+      <div className="flex flex-wrap gap-2">
+        {skillsData.map((skill, index) => (
+          <span
+            key={index}
+            className="bg-gray-500 text-gray-100 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300"
+          >
+            {skill}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Skills;
